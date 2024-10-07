c75962e25c7b3e209eb1a3e5e20acc6f8983caaa
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-10-07 10:18:02 +0200

    deploy

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   firebaseConfig: {
     apiKey: 'AIzaSyDMx5I4hVOmAFO0D2Mi-i-etHC-gOJ9_FY',
-    authDomain: 'fir-blog-9c514.firebaseapp.com',
+    authDomain: 'fir-blog-bc017.firebaseapp.com',
     projectId: 'fir-blog-bc017',
     storageBucket: 'fir-blog-bc017.appspot.com',
     messagingSenderId: '123456789012',

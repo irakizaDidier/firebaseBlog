4a86e07aa7080e4fb5694b22ba2c28b8ea22d5d1
irakizaDidier <didier.habimana@amalitechtraining.org>
2024-10-07 10:02:57 +0200

    adding login and signup

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyA5MXE32VQasBuvWdl_GIIzMciVxXAMDW4',
+    apiKey: 'AIzaSyDMx5I4hVOmAFO0D2Mi-i-etHC-gOJ9_FY',
     authDomain: 'fir-blog-9c514.firebaseapp.com',
-    projectId: 'fir-blog-9c514',
-    storageBucket: 'fir-blog-9c514.appspot.com',
+    projectId: 'fir-blog-bc017',
+    storageBucket: 'fir-blog-bc017.appspot.com',
     messagingSenderId: '123456789012',
     appId: '1:123456789012:web:abc123def456',
   },
